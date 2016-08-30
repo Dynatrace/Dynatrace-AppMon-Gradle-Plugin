0e80dbbe39e751d6f9d31a6c30c7ff44fe620dd6
Łukasz Hamerszmidt <lukasz.hamerszmidt@dynatrace.com>
2016-08-30 15:25:54 +0200

    Update Server REST SDK

---
+++ src/test/groovy/com/dynatrace/diagnostics/automation/gradle/DtClearSessionTest.groovy
@@ -0,0 +1,12 @@
+package com.dynatrace.diagnostics.automation.gradle
+
+
+class DtClearSessionTest extends spock.lang.Specification {
+    void setup() {
+
+    }
+
+    def "ExecuteTask"() {
+
+    }
+}
